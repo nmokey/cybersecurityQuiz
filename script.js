dcd893c9bd4790e4c24a1ac6a3f3f1f59a536cd7
Logan G <lg3139@pleasantonusd.net>
2023-09-05 19:34:15 -0700

    added score functionality to script.js

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 alert("Your connection is not private. Attackers may be trying to steal your data from ___.com (for example, passwords, messages, or credit cards. Learn more");
+
+var score;
+
+function addScore(){
+    score++;
+}
+
+function getScore(){
+    return score;
+}
