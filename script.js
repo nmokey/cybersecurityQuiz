4d7adf4013b61e35bba99c9b82f76fa79bef5ff9
Ryan Zheng <77017591+nmokey@users.noreply.github.com>
2023-09-07 10:48:46 -0700

    Initialize score variable

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 //document.getElementById("score").innerHTML = sessionStorage.score;
 
+
+sessionStorage.score = 0;
 function addScore() {
         sessionStorage.score = Number(sessionStorage.score) + 1;
     document.getElementById("score").innerHTML = sessionStorage.score;
